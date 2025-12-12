0ecb205af3ac018ae6987ce999e2a2b013206241
Neeraja <neerajamj0102@gmail.com>
2025-12-12 10:25:45 +0530

    Fix mealplan

--- models/mealPlanModel.js
+++ models/mealPlanModel.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const MealPlanSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   week: {
-    monday: { type: Object, default: null },
-    tuesday: { type: Object, default: null },
-    wednesday: { type: Object, default: null },
-    thursday: { type: Object, default: null },
-    friday: { type: Object, default: null },
-    saturday: { type: Object, default: null },
-    sunday: { type: Object, default: null }
+    monday: { type: Array, default: [] },
+    tuesday: { type: Array, default: [] },
+    wednesday: { type: Array, default: [] },
+    thursday: { type: Array, default: [] },
+    friday: { type: Array, default: [] },
+    saturday: { type: Array, default: [] },
+    sunday: { type: Array, default: [] },
   }
 }, { timestamps: true });
 
